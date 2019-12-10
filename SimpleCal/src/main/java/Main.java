8e03eab8994b667f3802368d38dba08d7a70f950
KagisoRKA <57135740+KagisoRKA@users.noreply.github.com>
2019-12-10 11:36:18 +0200

    Adding a complete gradle project

---
+++ SimpleCal/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SimpleCalculator.add(2,3,3));
+        System.out.println(SimpleCalculator.multiply(2,2,2));
+    }
+}
