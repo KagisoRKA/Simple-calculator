c073967201229ad0f716becfa772a0271b2e0b64
KagisoRKA <57135740+KagisoRKA@users.noreply.github.com>
2019-12-12 11:35:58 +0200

    Updating the file as per project structure requirements

--- SimpleCal/src/main/java/Calculator.java
+++ SimpleCal/src/main/java/Calculator.java
@@ -14,3 +14,9 @@ public class Calculator {
         return product;
     }
 }
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(2,3,3));
+        System.out.println(Calculator.multiply(2,2,2));
+    }
+}
