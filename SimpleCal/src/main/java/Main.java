39e96a7623ed20be15b19e4b855955fba503a645
KagisoRKA <57135740+KagisoRKA@users.noreply.github.com>
2019-12-12 11:35:03 +0200

    Deleting Main as per project structure requirements

--- SimpleCal/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(SimpleCalculator.add(2,3,3));
-        System.out.println(SimpleCalculator.multiply(2,2,2));
-    }
-}
