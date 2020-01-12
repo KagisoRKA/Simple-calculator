86ee1fc597eb48d2b0c4d516b129162981b9f40a
Kagiso Raseroka <kraseroka100@gmail.com>
2020-01-12 13:29:00 +0200

    Updating project structure

---
+++ src/main/java/MainProgram.java
@@ -0,0 +1,6 @@
+public class MainProgram {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(2, 3, 3));
+        System.out.println(Calculator.multiply(2, 2, 2));
+    }
+}
